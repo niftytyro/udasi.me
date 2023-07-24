49ba916f4901445b50f3365b4162d43b00825a44
Udasi Tharani <udasitharani@gmail.com>
2023-07-25 00:17:48 +0530

    setup home and add project card (WIP)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      black: "#101010",
+      white: "#FCFCFC",
+      gray: "#B9B9B9",
+    },
+    fontFamily: {
+      sans: ["Work Sans Variable", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
